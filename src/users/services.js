468ca34c187598f435133336ecee301d59bc7969
burhanazem <burhan12sab@gmail.com>
2023-04-23 16:48:35 +0300

    submit most feature

---
+++ src/users/services.js
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createUser = async (data) => {
+  return await prisma.user.create({
+    data,
+  });
+};
+
+export const login = async (username) => {
+  return username;
+};
+
+export const getUser = async (id) => {
+  return await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      tasks: true,
+      _count: {
+        select: {
+          tasks: true,
+        },
+      },
+    },
+  });
+};
